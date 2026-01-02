135fa97220b6494b39dfc61b1081a2a065b36f28
JohnSalinas123 <salinasjohn257@gmail.com>
2026-01-02 15:57:51 -0800

    add navbar component draft, links are not yet working

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import { Container, HStack, Link, Text } from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+
+import classes from "./Navbar.module.css";
+
+export function Navbar() {
+	return (
+		<Container className={classes.container}>
+			<HStack justify="space-between" gap={20} className="jetbrains-mono">
+				<HStack gap={8}>
+					<Link variant="plain">About me</Link>
+					<Link variant="plain">Experience</Link>
+					<Link variant="plain">Projects</Link>
+				</HStack>
+				<HStack>
+					<Text>John Salinas @ 2025</Text>
+				</HStack>
+			</HStack>
+		</Container>
+	);
+}
