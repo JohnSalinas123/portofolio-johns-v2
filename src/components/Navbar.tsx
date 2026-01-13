26af90018ba510bee26201a8eb7a03f0077e70d4
JohnSalinas123 <salinasjohn257@gmail.com>
2026-01-13 09:30:00 -0800

    add page and section links to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,17 @@
-import { Container, HStack, Icon, Text } from "@chakra-ui/react";
+import { Container, HStack, Icon, Link, Text } from "@chakra-ui/react";
 
 import classes from "./Navbar.module.css";
 
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import { useEffect, useRef, useState } from "react";
-import { NavLink } from "react-router";
+import { NavLink, useNavigate } from "react-router";
 
 export function Navbar() {
 	const lastScrollY = useRef(0);
 	const ticking = useRef(false);
 
-	// let location = useLocation();
+	const navigate = useNavigate();
 
 	const [hidden, setHidden] = useState(false);
 	const [scrolled, setScrolled] = useState(false);
@@ -44,10 +44,19 @@ export function Navbar() {
 		return () => window.removeEventListener("scroll", onScroll);
 	}, []);
 
-	const handleDynamicLinkClick = (pathname: string) => {
-		const element = document.getElementById(pathname);
-		element?.scrollIntoView(true);
-		element?.scrollIntoView({ behavior: "smooth" });
+	const handleNavClick = (anchor: string) => {
+		navigate("/");
+
+		setTimeout(() => {
+			const id = `${anchor}-section`;
+			const element = document.getElementById(id);
+			if (element) {
+				element.scrollIntoView({
+					behavior: "smooth",
+					block: "start",
+				});
+			}
+		}, 300);
 	};
 
 	return (
@@ -67,7 +76,7 @@ export function Navbar() {
 							About me
 						</NavLink>
 						<a
-							onClick={() => handleDynamicLinkClick("experience")}
+							onClick={() => handleNavClick("experience")}
 							className={classes.nav}
 						>
 							Experience
@@ -81,18 +90,41 @@ export function Navbar() {
 					</HStack>
 					<HStack>
 						<Text>John Salinas @ 2025</Text>
-						<Text>|</Text>
-						<Icon className={classes.icon}>
-							<FaLinkedin />
-						</Icon>
-						<Text>|</Text>
-						<Icon className={classes.icon}>
-							<IoIosMail />
-						</Icon>
-						<Text>|</Text>
-						<Icon className={classes.icon}>
-							<FaGithub />
-						</Icon>
+						<Text className={classes["text-ignore"]}>|</Text>
+						<Link
+							className={classes.media}
+							href="https://www.linkedin.com/in/johnsalinas123/"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<Icon className={classes.icon}>
+								<FaLinkedin />
+							</Icon>
+						</Link>
+
+						<Text className={classes["text-ignore"]}>|</Text>
+						<Link
+							className={classes.media}
+							href="mailto: salinasjohn257@gmail.com"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<Icon className={classes.icon}>
+								<IoIosMail />
+							</Icon>
+						</Link>
+
+						<Text className={classes["text-ignore"]}>|</Text>
+						<Link
+							className={classes.media}
+							href="https://github.com/JohnSalinas123"
+							target="_blank"
+							rel="noreferrer"
+						>
+							<Icon className={classes.icon}>
+								<FaGithub />
+							</Icon>
+						</Link>
 					</HStack>
 				</HStack>
 			</Container>
