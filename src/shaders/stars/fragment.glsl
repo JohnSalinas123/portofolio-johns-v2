cbcb13dff3ab6ccff7f78d35eb68fe84a772a5a6
JohnSalinas123 <salinasjohn257@gmail.com>
2026-02-04 09:46:58 -0800

    fixed stars having pure black border that stood out against bg

--- src/shaders/stars/fragment.glsl
+++ src/shaders/stars/fragment.glsl
@@ -8,13 +8,9 @@ void main() {
     strength = 1.0 - strength;
     strength = pow(strength, 10.0);
 
-    // Final color
-    vec3 color = mix(vec3(0.0), vec3(1.0), strength);
+    vec3 color = vec3(1.0);
 
-    if (strength < 0.01)
-        discard;
-
-    gl_FragColor = vec4(color,1.0);
+    gl_FragColor = vec4(color,strength);
 
     #include <colorspace_fragment>
 }
